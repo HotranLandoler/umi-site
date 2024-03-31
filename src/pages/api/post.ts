f3fdb9bc7c56575582c7ccc1f5ffe459f9af3aca
LOU Chaoli <ex012@qq.com>
2024-03-31 13:13:23 +0800

    Delete files

--- src/pages/api/post.ts
+++
@@ -1,7 +0,0 @@
-import type { APIContext, APIRoute } from "astro"
-
-import { posts } from "~/data/post"
-
-export const GET: APIRoute = async function getPosts(context: APIContext) {
-  return new Response(JSON.stringify(posts))
-}
