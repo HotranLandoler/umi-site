b08c9426ebfe7025f6a3a973711f3de5b3ff1eb6
LOU Chaoli <ex012@qq.com>
2024-04-03 14:23:24 +0800

    Add example image

--- components/game-card.tsx
+++ components/game-card.tsx
@@ -5,14 +5,18 @@ export default function GameCard({ gameMeta }: { gameMeta: GameMeta }) {
   return (
     <figure className="relative overflow-hidden rounded-lg">
       <Image
-        className="w-full bg-slate-200"
-        src={gameMeta.imageSrc}
-        width={100}
-        height={100}
+        className="h-full w-full bg-slate-200 object-cover"
+        src={
+          gameMeta.imageSrc != ""
+            ? gameMeta.imageSrc
+            : "/examples/IntoSoil.webp"
+        }
+        width={300}
+        height={300}
         alt={gameMeta.name}
       />
       {gameMeta.isNew && (
-        <div className="absolute -left-1 top-0 rotate-12 bg-yellow-400 px-4 py-2">
+        <div className="absolute -left-1 top-0 bg-yellow-400 px-4 py-2">
           新品
         </div>
       )}
