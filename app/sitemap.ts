89aff042be4738441dc12caba02deafc9a24c1f7
LOU Chaoli <ex012@qq.com>
2024-04-21 15:38:37 +0800

    Setup sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,19 @@
-export const baseUrl = "https://umigames.info";
+import { getAllGames } from "@/lib/data";
+
+export const baseUrl = "https://umi-site.pages.dev/";
+
+export default async function sitemap() {
+  let now = new Date().toISOString().split("T")[0];
+
+  let games = getAllGames().map((game) => ({
+    url: `${baseUrl}/games/${game.slug}`,
+    lastModified: now,
+  }));
+
+  let routes = ["", "/games"].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: now,
+  }));
+
+  return [...routes, ...games];
+}
