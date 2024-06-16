8c08fa3f4a219a9005b8fe6694cbb432693f23f0
LOU Chaoli <ex012@qq.com>
2024-06-16 16:16:47 +0800

    chore: hide like button

--- app/(main)/posts/[id]/page.tsx
+++ app/(main)/posts/[id]/page.tsx
@@ -27,7 +27,7 @@ export default async function PostPage({ params: { id } }: Props) {
       <h1 className="mb-4 text-3xl font-bold">{post.title}</h1>
       <TipTapRenderer content={post.content as JSONContent} />
       <footer className="mt-8 border-t-2 border-muted">
-        <LikeButton postId={post.id} initialLikes={post._count.likes} />
+        {/* <LikeButton postId={post.id} initialLikes={post._count.likes} /> */}
       </footer>
     </article>
   );
